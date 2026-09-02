8be20fe3585fd4899d411530d07b39c246ea919d
Abhimanyu Tiwari <abhimanyu.tiwari@joshtechnologygroup.com>
2026-09-02 15:10:42 +0530

    feat(discount): add DISCOUNT_CONFIG constants and discount eligibility limit

--- src/config/constants.js
+++ src/config/constants.js
@@ -20,6 +20,7 @@ module.exports = {
     MAX_ITEMS_PER_ORDER: 50,
     MIN_ORDER_AMOUNT: 1.00,
     MAX_ORDER_AMOUNT: 50000.00,
+    MIN_DISCOUNT_ELIGIBLE_AMOUNT: 25.00,
     CANCELLATION_WINDOW_HOURS: 24,
   },
 
@@ -40,6 +41,18 @@ module.exports = {
     'sports', 'food-beverage', 'beauty', 'toys',
   ],
 
+
+  // --- Discount Codes ---
+  DISCOUNT_CONFIG: {
+    MAX_DISCOUNT_PERCENTAGE: 75,
+    MIN_ORDER_FOR_DISCOUNT: 25.00,
+    CODES: {
+      WELCOME10: { type: 'percentage', value: 10, description: 'Welcome offer - 10% off' },
+      SAVE20: { type: 'percentage', value: 20, description: 'Loyalty reward - 20% off' },
+      FREESHIP: { type: 'fixed', value: 9.99, description: 'Free shipping coupon' },
+      VIP50: { type: 'percentage', value: 50, description: 'VIP member discount - 50% off' },
+    },
+  },
   // --- Rate Limiting ---
   RATE_LIMIT: {
     WINDOW_MS: 15 * 60 * 1000,  // 15 minutes
@@ -50,3 +63,4 @@ module.exports = {
   // --- Cache ---
   CACHE_TTL_SECONDS: 60,
 };
+
